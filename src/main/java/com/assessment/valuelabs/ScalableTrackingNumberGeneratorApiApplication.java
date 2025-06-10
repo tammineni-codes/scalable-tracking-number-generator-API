5be9de5ba99cc73c646cb44d65eb283d097b01ac
tammineni-codes <niranjan.tammineni@gmail.com>
2025-06-10 18:36:26 +0530

    Project initialization

---
+++ src/main/java/com/assessment/valuelabs/ScalableTrackingNumberGeneratorApiApplication.java
@@ -0,0 +1,13 @@
+package com.assessment.valuelabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScalableTrackingNumberGeneratorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScalableTrackingNumberGeneratorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assessment/valuelabs/ScalableTrackingNumberGeneratorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assessment.valuelabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScalableTrackingNumberGeneratorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
